6e130bdaf4080d03cce6ec9625520362f9af3536
Kaizen Aurelius <rafaeltobass@gmail.com>
2026-03-20 13:33:23 -0500

    Funcion para crear los elementos HTML de cada post Esta funcion sera llamada por la funciton que hace la peticion GET a la API, y se le pasara el post que se quiere mostrar

--- app.js
+++ app.js
@@ -8,6 +8,29 @@ function sendHTTPRequest(method, url, data) {
     }).then((response) => {return response.json()});
 };
 
+
+// Funcion que crea el esqueleto del post y se ejecutara en el for de la funcion de renderizado
+
+function createPostsElements(post){ //elemento post es la respuesta del get, es decir data
+
+    const postContainer = document.createElement('article');
+    postContainer.id = post.id
+    postContainer.classList.add("post-item");
+
+    const postTitle = document.createElement('h2');
+    postTitle.textContent = post.title
+
+    const postBody = document.createElement('p');
+    postBody.textContent = post.body
+
+    const postDeleteButton = document.createElement('button')
+    postDeleteButton.textContent = "DELETE Post"
+
+    postContainer.append(postTitle, postBody, postDeleteButton);
+
+    return postContainer;
+
+}
 async function getPosts() {
 
     const data = await sendHTTPRequest("GET", 'https://jsonplaceholder.typicode.com/posts');
