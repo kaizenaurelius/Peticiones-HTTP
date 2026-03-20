b120589e78495df4947aee6d00189e4026881994
Kaizen Aurelius <rafaeltobass@gmail.com>
2026-03-20 13:04:09 -0500

    Creacion de funcion para hacer peticiones HTTP

---
+++ app.js
@@ -0,0 +1,9 @@
+function sendHTTPRequest(method, url, data) {
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then((response) => {return response.json()});
+};
